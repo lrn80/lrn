09c998ff01fa28d41a4eef6be6a44e55d6ff5423
lrn80 <1349539656@qq.com>
2021-05-08 19:48:34 +0800

    {

--- application/api/model/Detail.php
+++ application/api/model/Detail.php
@@ -8,13 +8,13 @@ class Detail extends BaseModel
 {
     const CAN_LEAD = 1;
     const CAN_NOT_LEAD = 0;
-    public function getStatusAttr($value){
-        if ($value == self::CAN_LEAD) {
-            $value = '可借阅';
-        } else {
-            $value = '不可借阅';
-        }
-
-        return $value;
-    }
+//    public function getStatusAttr($value){
+//        if ($value == self::CAN_LEAD) {
+//            $value = '可借阅';
+//        } else {
+//            $value = '不可借阅';
+//        }
+//
+//        return $value;
+//    }
 }
